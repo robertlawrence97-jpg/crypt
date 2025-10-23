0dd3cce4f31d9f281260b7dc556ac9f13b9e88ed
robertlawrence97-jpg <robert.lawrence97@gmail.com>
2025-10-23 00:54:03 -0400

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './keg_tracker_enhanced.tsx'
+import App from './keg_tracker_enhanced'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
